826cc2e6d3381d35b52a03c8b7831497b738a9e5
darklinden <darklinden@gmail.com>
2020-06-23 14:21:22 +0800

    bug fixes

--- 28/test.py
+++ 28/test.py
@@ -14,28 +14,53 @@ def main():
 
     test_case_list = [
         {
-            'test_args': {'haystack': '1234567893450', 'needle': '345'},
-            'expect_val': {'ret_val': 2},
+            'test_args': {
+                'haystack': '1234567893450',
+                'needle': '345'
+            },
+            'expect_val': {
+                'ret_val': 2
+            },
             'comment': '无重复'
         },
         {
-            'test_args': {'haystack': '123434134343434134341', 'needle': '34341'},
-            'expect_val': {'ret_val': 2},
+            'test_args': {
+                'haystack': '123434134343434134341',
+                'needle': '34341'
+            },
+            'expect_val': {
+                'ret_val': 2
+            },
             'comment': '有重复'
         },
         {
-            'test_args': {'haystack': 'asfeqewqfewqfewqffds', 'needle': '34341'},
-            'expect_val': {'ret_val': -1},
+            'test_args': {
+                'haystack': 'asfeqewqfewqfewqffds',
+                'needle': '34341'
+            },
+            'expect_val': {
+                'ret_val': -1
+            },
             'comment': '不存在'
         },
         {
-            'test_args': {'haystack': 'asfeqewqfewqfewqffds', 'needle': ''},
-            'expect_val': {'ret_val': 0},
+            'test_args': {
+                'haystack': 'asfeqewqfewqfewqffds',
+                'needle': ''
+            },
+            'expect_val': {
+                'ret_val': 0
+            },
             'comment': '空输入'
         },
         {
-            'test_args': {'haystack': '1234', 'needle': 'abcdefghijklmnopqrstuvwxyz'},
-            'expect_val': {'ret_val': -1},
+            'test_args': {
+                'haystack': '1234',
+                'needle': 'abcdefghijklmnopqrstuvwxyz'
+            },
+            'expect_val': {
+                'ret_val': -1
+            },
             'comment': '长needle'
         },
     ]
@@ -48,10 +73,10 @@ def main():
         for ins in instance_list:
             c = Compare(t['comment'])
             c.clazz = type(ins).__name__
-            c.time_cost = a.test(**t)
+            c.time_cost = ins.test(**t)
             test_list.append(c)
 
-    test_list.sort(key=lambda Compare: Compare.time_cost)
+    test_list.sort(key=lambda compare: compare.time_cost)
 
     for one in test_list:
         print(one)
